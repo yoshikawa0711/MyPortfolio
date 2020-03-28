7c3af9304b9e32700daaccd01c8c6778739e311b
yoshikawa0711 <b1017166@fun.ac.jp>
2020-03-28 13:58:02 +0900

    [add]マウスオーバーした時に文字の色を変更

--- tab_action.js
+++ tab_action.js
@@ -1,7 +1,9 @@
 function tabOver(item) {
     item.style.backgroundColor = "#E5CA9A";
+    item.style.color = "#330000";
 }
 
 function tabLeave(item) {
     item.style.backgroundColor = "#330000";
+    item.style.color = "#E5CA9A";
 }
